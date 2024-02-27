2abd21fc923457696631d3d29a639cf6510c13b0
huangyukai <Wekohyk@163.com>
2024-02-27 14:50:52 +0800

    Add server configuration option to automatically open browser on server start

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Server configuration options.
+  server: {
+    // Whether to open the browser automatically when the server starts.ƒ
+    open: true,
+  },
 });
