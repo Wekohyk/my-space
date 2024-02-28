c577f85bd13342d70bf57e4cd633a2d9ae74189e
huangyukai <Wekohyk@163.com>
2024-02-28 09:26:25 +0800

    Update home page layout

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center items-center">
+  <div class="flex justify-center items-center h-100vh">
     <div class="font-PacificoRegular">{{ $t('wekoHome') }}</div>
   </div>
 </template>
