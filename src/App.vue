3fddd68455485c5c8bcb3e6acc2c010adb99d44e
huangyukai <Wekohyk@163.com>
2024-02-28 09:54:50 +0800

    Remove mask transition and update home page layout

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <template>
-  <transition
-    enter-from-class="opacity-0"
-    enter-to-class="opacity-100"
-    leave-from-class="opacity-100"
-    leave-to-class="opacity-0"
-  >
-    <div
-      class="fixed top-0 left-0 right-0 bottom-0 bg-[var(--background)] opacity-70 transition-opacity duration-300 ease-in -z-9999999999"
-      v-if="mask"
-    ></div>
-  </transition>
   <router-view></router-view>
 </template>
 
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
-
-const mask = ref(false);
-onMounted(() => {
-  mask.value = true;
-});
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss"></style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="flex justify-center items-center h-100vh">
-    <div class="font-PacificoRegular text-[var(--text)]">
-      {{ $t('wekoHome') }}
+  <!-- The mask is a semi-transparent layer that covers the entire screen. -->
+  <transition
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
+  >
+    <div
+      v-if="mask"
+      class="fixed top-0 left-0 right-0 bottom-0 bg-[var(--background)] opacity-70 transition-opacity duration-300 ease-in"
+    ></div>
+  </transition>
+
+  <div class="fixed top-0 right-0 bottom-0 left-0">
+    <div class="flex justify-center items-center h-100vh flex-col">
+      <div class="font-PacificoRegular text-[var(--text)]">
+        {{ $t('wekoHome') }}
+      </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const mask = ref(false);
+onMounted(() => {
+  mask.value = true;
+});
+</script>
 
 <style lang="scss" scoped></style>
