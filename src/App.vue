678c61bf1c37b596ff81684b950cf08d8982e20d
huangyukai <Wekohyk@163.com>
2024-02-28 09:47:55 +0800

    Update styles to support light and dark mode

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     leave-to-class="opacity-0"
   >
     <div
-      class="fixed top-0 left-0 right-0 bottom-0 bg-black/70 transition-opacity duration-300 ease-in -z-9999999999"
+      class="fixed top-0 left-0 right-0 bottom-0 bg-[var(--background)] opacity-70 transition-opacity duration-300 ease-in -z-9999999999"
       v-if="mask"
     ></div>
   </transition>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex justify-center items-center h-100vh">
-    <div class="font-PacificoRegular">{{ $t('wekoHome') }}</div>
+    <div class="font-PacificoRegular text-[var(--text)]">
+      {{ $t('wekoHome') }}
+    </div>
   </div>
 </template>
 
