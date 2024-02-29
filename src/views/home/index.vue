6f06d7acced934c40a3423a975921bdbf1adc672
huangyukai <Wekohyk@163.com>
2024-02-29 09:18:18 +0800

    Add image transition to home page

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -11,7 +11,20 @@
       class="fixed top-0 left-0 right-0 bottom-0 bg-[var(--background)] opacity-70 transition-opacity duration-300 ease-in"
     ></div>
   </transition>
-
+  <!-- <img
+    src="https://tuapi.eees.cc/api.php?category=fengjing&type=302&px="
+    alt=""
+  /> -->
+  <transition
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
+  >
+    <div v-if="mask">
+      <img src="https://tuapi.eees.cc/api.php?category=fengjing&type=302&px=" />
+    </div>
+  </transition>
   <div class="fixed top-0 right-0 bottom-0 left-0">
     <div class="flex justify-center items-center h-100vh">
       <!-- left box -->
