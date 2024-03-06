cd051a49922c918c18ab444144a9682ab37bdbc3
huangyukai <Wekohyk@163.com>
2024-03-06 14:51:35 +0800

    Add Total component and implement mouseenter and mouseleave events

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <Loading></Loading>
   <BackgroundImg @loadComplete="loadComplete"></BackgroundImg>
   <Mousemove v-if="toggleEffect"></Mousemove>
+  <Total :title="local.message1" :visible="visible"></Total>
   <div>
+    <!-- drawer -->
     <div class="boxDrawer" v-if="drawer">
       <LineCombination></LineCombination>
     </div>
-
+    <!-- mid -->
     <MyMessage></MyMessage>
+    <!-- button -->
     <div
       class="absolute top-10vh right-5vh w-4rem h-4rem flex justify-center items-center flex-col gap-10"
     >
-      <div>
+      <div @mouseenter="mouseenter" @mouseleave="mouseleave">
         <Switch
           :width="76"
           :height="34"
@@ -48,15 +51,32 @@ import Loading from './components/Loading.vue';
 import Mousemove from './components/Mousemove.vue';
 import LineCombination from './components/LineCombination.vue';
 import Switch from './components/Switch.vue';
+import Total from './components/Total.vue';
+import isEn from './lang/index';
+
+const local = isEn
+  ? {
+      message1: '是否显示',
+    }
+  : {
+      message1: 'Whether to display',
+    };
 
 const drawer = ref<boolean>(false);
+const visible = ref<boolean>(false);
 const handleSwitchChange = (value: boolean) => {
   drawer.value = !value;
   console.log('drawer.value', drawer.value);
 };
+const mouseenter = () => {
+  visible.value = true;
+};
+const mouseleave = () => {
+  visible.value = false;
+};
+
 // toggle effect
 const toggleEffect = ref<boolean>(true);
-
 // loading complete event
 const loadComplete = () => {
   nextTick(() => {
---
+++ src/components/Total.vue
@@ -0,0 +1,28 @@
+<template>
+  <transition
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
+  >
+    <div
+      v-if="props.visible"
+      class="w-full flex justify-center h-15vh items-center transition-opacity duration-300 ease-in"
+    >
+      <div
+        class="fixed flex justify-center items-center w-auto h-auto bg-#000 px-1vw py-2vh rounded-15"
+      >
+        <div class="text-#fff">{{ props.title }}</div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  visible: boolean;
+  title: string;
+}>();
+</script>
+
+<style lang="scss" scoped></style>
