edc78f213ce1a36d6bd2be289cb47a486be50582
huangyukai <Wekohyk@163.com>
2024-03-06 15:25:40 +0800

    Add animation to Total component

--- src/components/Total.vue
+++ src/components/Total.vue
@@ -12,7 +12,7 @@
       <div
         class="fixed flex justify-center items-center w-auto h-auto bg-#000 px-1vw py-1vh rounded-15"
       >
-        <div class="text-#fff">{{ props.title }}</div>
+        <div class="animation" :data-text="props.title">{{ props.title }}</div>
       </div>
     </div>
   </transition>
@@ -25,4 +25,36 @@ const props = defineProps<{
 }>();
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.animation {
+  color: #222;
+  position: relative;
+  &::after {
+    content: attr(data-text);
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-image: linear-gradient(
+      to right,
+      rgb(236, 72, 153),
+      rgb(239, 68, 68),
+      rgb(234, 179, 8)
+    );
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+    clip-path: ellipse(2vw 2vw at 0% 50%);
+    animation: move 8s ease-in-out infinite;
+  }
+}
+
+@keyframes move {
+  50% {
+    clip-path: ellipse(2vw 2vw at 100% 50%);
+  }
+
+  100% {
+    clip-path: ellipse(2vw 2vw at 0% 50%);
+  }
+}
+</style>
